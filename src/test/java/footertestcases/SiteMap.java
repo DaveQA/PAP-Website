5a106d3a1a98ad77e5a34de76cb250214a5da6c1
DaveQA <deholliday487@gmail.com>
2018-01-04 10:14:36 -0500

    Added more Or statements to the site map check for test data

--- src/test/java/footertestcases/SiteMap.java
+++ src/test/java/footertestcases/SiteMap.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import java.util.List;
 
 public class SiteMap {
@@ -29,21 +30,21 @@ public class SiteMap {
         List<WebElement> linkSize = driver.findElements(By.cssSelector(".sitemap-section a"));
         linksCount = linkSize.size();
 //        System.out.println("Total number of links on the site map: "+linksCount);
-        links= new String[linksCount];
+        links = new String[linksCount];
 //        System.out.println("List of links Available: ");
 
-        for(int i=0;i<linksCount;i++)
-        {
+        for (int i = 0; i < linksCount; i++) {
             links[i] = linkSize.get(i).getAttribute("href");
-//            System.out.println(linksize.get(i).getAttribute("href"));
+//            System.out.println(linkSize.get(i).getAttribute("href"));
         }
         System.out.println("===================================================================================");
         System.out.println("Checking site for testing data...");
-        for(int i=0;i<linksCount;i++)
-        {
+        for (int i = 0; i < linksCount; i++) {
             driver.navigate().to(links[i]);
             Thread.sleep(1000);
-            if(driver.getPageSource().contains("TEST") ||driver.getPageSource().contains(" test ")){
+            if (driver.getPageSource().contains("TEST") || driver.getPageSource().contains(" test ")
+                    || driver.getPageSource().contains(" Test")
+                    || driver.getPageSource().contains(" Test")) {
                 System.out.println(ANSI_RED + "Page contains test data, please check the site" + ANSI_RESET);
                 System.out.println(ANSI_RED + driver.getCurrentUrl() + ANSI_RESET);
             }
@@ -52,7 +53,7 @@ public class SiteMap {
         System.out.println("===================================================================================");
     }
 
-    private void goToSiteMapPage(WebDriver driver1){
+    private void goToSiteMapPage(WebDriver driver1) {
         driver = driver1;
         List<WebElement> scrollToPoint = driver.findElements(By.cssSelector(".footer-nav a"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrollToPoint.get(8));
